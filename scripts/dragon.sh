0a45d2c9e8e051a5966fcf3296b6d67c59f15d7b
Andrei Maximov <andrei.maximov@gmail.com>
2017-06-22 12:10:09 -0700

    Update glass dragon download address

--- scripts/dragon.sh
+++ scripts/dragon.sh
@@ -13,7 +13,7 @@ if [ ! -f ${EXE} ]; then
 fi
 
 OBJ="${CACHE}/CornellBox-Dragon.obj"
-URL="https://s3.amazonaws.com/andrei-maximov-public/ucsd/cse163/pa3/scenes/CornellBox-Dragon.obj"
+URL="http://andreimaximov.com/files/projects/pathtracer/CornellBox-Dragon.obj"
 
 if [[ ! -f ${OBJ} || ! -d ${CACHE} ]]; then
     echo "Downloading OBJ file..."
